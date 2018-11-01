c9fe11ac7201a5aca98d01ef5fb12e26c2e74218
tspeu <tenshotseuropa@gmail.com>
2018-11-01 09:51:03 +0000

    add logic test post

--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -10,24 +11,72 @@ import { HttpClient } from '@angular/common/http';
 export class ValueComponent implements OnInit {
 
   values: any;
+  url = 'http://localhost:5000/api/values';
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
     console.log('Init data-->');
-    this.getValues();
+    this.postTest();
+    // this.getValues();
   }
 
   getValues() {
 
-    const url = 'http://localhost:5000/api/values';
 
-    this.http.get(url).subscribe(
+    this.http.get(this.url).subscribe(
       res => {
         this.values = res;
       }, error => { console.log(error);
         });
+
+  }
+
+
+  postTest() {
+    console.log('desde api result--->');
+    // const url = 'http://localhost:5000/api/value';
+    const testData = {
+          'VehicleId' : 12,
+          'Type' : '',
+          'ManufacturerNameShort' : 'ASDASDADS',
+          'Price' :  10
+        };
+
+
+    const body = JSON.stringify(testData);
+    console.log('desde api result--->', testData);
+    console.log('desde api result--con ->', JSON.stringify(testData));
+
+    // let dataFormat = JSON.stringify(testData);
+    const headerOptions = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+      return this.http.post(this.url, body, {headers: headerOptions})
+            .subscribe( res => {
+              this.values = res;
+                console.log('desde la api debe venir --->',  res);
+                console.log('desde la api debe venir ---  this.value>',  this.values);
+                console.log('desde la api debe venir ---  this.value-vehicleId>',  this.values.vehicleId);
+                console.log('desde la api debe venir ---  this.value-returnCode>',  this.values.returnCode);
+
+              if (this.values.returnCode) {
+                console.log('VALIDO');
+              } else {
+                console.log('INVALIDO');
+
+              }
+
+      }, error => { console.log(error);
+      });
+
+
+  //  .pipe(
+  //     map((res: any) => {
+  //       const result = res;
+  //       console.log('desde api result--->', result);
+  //     })
+  //   );
 
   }
 
